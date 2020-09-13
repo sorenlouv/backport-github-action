dbb43c1f7384b3c9fe82cc82179f5637e3e37106
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-09-13 09:43:04 +0200

    Debug payload

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ export async function main(
   payload: EventPayloads.WebhookPayloadPullRequest,
   actor: Context['actor']
 ) {
+  console.log(payload);
   if (!payload.pull_request) {
     throw new Error('Pull request payload unavailable');
   }
